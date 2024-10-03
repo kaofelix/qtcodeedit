4c908381341a995a282a4e37a36d2a1382cefc7b
Kao Félix <kcfelix@gmail.com>
2024-10-03 19:23:51 +0200

    Add line numbers

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
-# CodeEdit for Python Qt
+# CodeEdit for Qt and Python
 
-This is a QPlainTextEdit based widget with added syntax highlight with
-multiple languages and themes. Language support is provided by
-Pygments, so all languages and themes supported by it are supported.
+This is a simple code editor widget for Python and Qt supporting multiple languages and color
+themes.
+
+## Features
+
+- Syntax and theming support powered by [Pygments](https://pygments.org), so any language and theme
+  supported by it can be used
+- Line numbers
+
+## Implementation
+
+The CodeEdit widget is based on `QPlainTextEdit` and syntax highlight is provided by a Pygments
+based implementation of `QSyntaxHighlighter`. The line number implementation was based on [this
+sample](https://doc.qt.io/qt-5/qtwidgets-widgets-codeeditor-example.html) from the Qt docs, with a
+bit of cleanup and refactoring on my part.
--- qtcodeedit.py
+++ qtcodeedit.py
@@ -4,9 +4,16 @@ from pygments import lex
 from pygments.lexers import get_all_lexers, get_lexer_by_name
 from pygments.styles import get_all_styles, get_style_by_name
 from pygments.token import Token
-from PySide6.QtCore import Signal
-from qtpy.QtGui import QColor, QFont, QPalette, QSyntaxHighlighter, QTextCharFormat
-from qtpy.QtWidgets import QPlainTextEdit
+from qtpy.QtCore import QEvent, QObject, QRect, Qt, Signal
+from qtpy.QtGui import (
+    QColor,
+    QFont,
+    QPainter,
+    QPalette,
+    QSyntaxHighlighter,
+    QTextCharFormat,
+)
+from qtpy.QtWidgets import QPlainTextEdit, QWidget
 
 
 class Language(NamedTuple):
@@ -27,6 +34,8 @@ class CodeEdit(QPlainTextEdit):
         self.highlighter = PygmentsSyntaxHighlighter(self.document())
         self.highlighter.styleChanged.connect(self._updatePalette)
         self._updatePalette()
+
+        self.lineNumberArea = LineNumberArea(self)
 
     def languages(self):
         return (
@@ -106,3 +115,68 @@ class PygmentsSyntaxHighlighter(QSyntaxHighlighter):
             return fmt
 
         return {token_type: to_fmt(token_style) for token_type, token_style in style}
+
+
+class LineNumberArea(QWidget):
+    MIN_DIGITS = 3
+    MARGIN_RIGHT = 3
+
+    def __init__(self, editor: "CodeEdit"):
+        super().__init__(editor)
+        self.editor = editor
+        self.editor.installEventFilter(self)
+        self.editor.updateRequest.connect(self._onEditorUpdateRequest)
+        self.editor.blockCountChanged.connect(self._updateWidth)
+        self._updateWidth(self.editor.blockCount())
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.fillRect(event.rect(), self.palette().base())
+
+        block = self.editor.firstVisibleBlock()
+        top = self._topOfBlock(block)
+        bottom = top + self._blockHeight(block)
+
+        while block.isValid() and top <= event.rect().bottom():
+            if block.isVisible() and bottom >= event.rect().top():
+                lineNumber = str(block.blockNumber() + 1)
+                self._paintNumber(painter, lineNumber, top)
+
+            block = block.next()
+            top = bottom
+            bottom = top + self._blockHeight(block)
+
+    def eventFilter(self, watched: QObject, event: QEvent):
+        if watched is not self.editor and event.type() != QEvent.Type.Resize:
+            return False
+
+        cr = self.editor.contentsRect()
+        self.setGeometry(cr.left(), cr.top(), self.width(), cr.height())
+        return False
+
+    def _updateWidth(self, lineCount):
+        digits = max(self.MIN_DIGITS, len(str(lineCount)))
+        charWidth = self.fontMetrics().horizontalAdvance("9")
+        space = 3 + self.MARGIN_RIGHT + charWidth * digits
+        self.setFixedWidth(space)
+        self.editor.setViewportMargins(space, 0, 0, 0)
+
+    def _onEditorUpdateRequest(self, rect, dy):
+        if dy:
+            self.scroll(0, dy)
+        else:
+            self.update(0, rect.y(), self.width(), rect.height())
+
+    def _topOfBlock(self, block):
+        blockGeom = self.editor.blockBoundingGeometry(block)
+        return int(round(blockGeom.translated(self.editor.contentOffset()).top()))
+
+    def _blockHeight(self, block):
+        return int(round(self.editor.blockBoundingRect(block).height()))
+
+    def _paintNumber(self, painter, number, top):
+        painter.setPen(self.palette().placeholderText().color())
+        textRect = QRect(
+            0, top, self.width() - self.MARGIN_RIGHT, self.fontMetrics().height()
+        )
+        painter.drawText(textRect, Qt.AlignmentFlag.AlignRight, number)
